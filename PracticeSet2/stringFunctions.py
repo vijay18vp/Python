f8eece26abf6c74f55538396f4e14bd8888e0933
vijay18vp <voidprotocol18@gmail.com>
2025-09-18 00:12:36 +0530

    Learning string

---
+++ PracticeSet2/stringFunctions.py
@@ -0,0 +1,9 @@
+# # to check endsWith
+# str = input("Enter Anything : ")
+# print(str.endswith("jay"))
+
+
+# to count the occurence of any letter 
+str1 = input("Enter anything : ")
+count = str1.count("a")
+print(count)
