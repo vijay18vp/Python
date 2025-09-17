46a7e81da8addcf9f52f09c0d35d2e85680f7735
vijay18vp <voidprotocol18@gmail.com>
2025-09-17 23:47:31 +0530

    Learning

---
+++ PracticeSet1/average.py
@@ -0,0 +1,5 @@
+number1 = int(input("Enter a first number : "))
+number2 = int(input("Enter a second number : "))
+
+average = (number1 + number2) / 2
+print("The average of two numbers is : " , average )
---
+++ PracticeSet1/square.py
@@ -0,0 +1,5 @@
+number = int(input("Enter a number : "))
+def square(number):
+    return number * number 
+
+print("The square of the given number is : " , square(number))
